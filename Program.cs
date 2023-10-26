340b5b40443b86775ae80177f046a5cfb8fdd6c7
Leonardo-Cosmos <kylin_cl@hotmail.com>
2023-10-26 11:15:34 +0800

    Implement save and restore by record file

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿/* 2023/10/26 */
+using FileAttributeRecorder.Info;
+
+string opearation;
+string dirPath;
+if (args.Length == 0)
+{
+    Console.WriteLine("No operation specified.");
+    return;
+}
+else
+{
+    opearation = args[0];
+}
+
+if (args.Length > 1)
+{
+    dirPath = args[1];
+}
+else
+{
+    dirPath = ".";
+}
+
+var fileInfoTool = new FileInfoTool(dirPath);
+switch (opearation)
+{
+    case "-s":
+        fileInfoTool.SaveFileDate();
+        break;
+    case "-r":
+        fileInfoTool.RestoreFileDate();
+        break;
+    default:
+        Console.WriteLine("No valid operation specified.");
+        break;
+}
